288dbfdaee165561711bfd09c4c071eea06f2ef0
Lee Davis <leedavis81@hotmail.com>
2013-03-04 19:47:20 +0000

    update

--- library/Drest/Router.php
+++ library/Drest/Router.php
@@ -1,9 +1,21 @@
 <?php
 namespace Drest;
 
+use Doctrine\Tests\Common\Annotations\Fixtures\Annotation\Route;
 
-class Router
+use	Symfony\Component\Routing\Router,
+	Symfony\Component\Routing\RouteCollection,
+	Symfony\Component\Routing\Route;
+
+/**
+ *
+ * Drest Router extends symfony route collection
+ * @author Lee
+ * @todo: create an adapter for other fw's - this would then make it possible to handle dispatch() from fw specific controllers
+ */
+class Router extends Router
 {
+
 
 
 
