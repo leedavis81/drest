3613df23af757a5ebad3b1bca43f0957d3dc3c35
Lee Davis <leedavis81@hotmail.com>
2015-07-27 17:04:34 +0100

    Abstract PUT/PATCH logic into AbstractAction class

--- src/Drest/Service/Action/AbstractAction.php
+++ src/Drest/Service/Action/AbstractAction.php
@@ -303,6 +303,53 @@ abstract class AbstractAction
 
 
     /**
+     * Perform a default update action. Logic between PUT and PATCH (not POST) are identical
+     * If things change then we can start to break this function down
+     * @return ResultSet
+     */
+    protected function performDefaultUpdateAction()
+    {
+        $matchedRoute = $this->getMatchedRoute();
+        $classMetaData = $matchedRoute->getClassMetaData();
+        $elementName = $classMetaData->getEntityAlias();
+
+        $em = $this->getEntityManager();
+
+        $qb = $em->createQueryBuilder()->select($elementName)->from($classMetaData->getClassName(), $elementName);
+        foreach ($matchedRoute->getRouteParams() as $key => $value) {
+            $qb->andWhere($elementName . '.' . $key . ' = :' . $key);
+            $qb->setParameter($key, $value);
+        }
+
+        try {
+            $object = $qb->getQuery()->getSingleResult(ORM\Query::HYDRATE_OBJECT);
+        } catch (\Exception $e) {
+            return $this->handleError($e, Response::STATUS_CODE_404);
+        }
+
+        $this->runHandle($object);
+
+        // Attempt to save the modified resource
+        try {
+            $em->persist($object);
+            $em->flush($object);
+
+            $location = $matchedRoute->getOriginLocation(
+                $object,
+                $this->getRequest()->getUrl(),
+                $this->getEntityManager()
+            );
+            $this->getResponse()->setStatusCode(Response::STATUS_CODE_200);
+            $resultSet = ResultSet::create(array('location' => ($location) ? $location : 'unknown'), 'response');
+        } catch (\Exception $e) {
+            return $this->handleError($e, Response::STATUS_CODE_500);
+        }
+
+        return $resultSet;
+    }
+
+
+    /**
      * Run the handle call on an entity object
      * @param object $object
      */
