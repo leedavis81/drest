88ab848ae5b0a1ca817633e1ff1460527931937d
Lee Davis <leedavis81@hotmail.com>
2015-07-30 11:13:00 +0100

    Add a few more tests

--- tests/DrestTests/Functional/ManagerTest.php
+++ tests/DrestTests/Functional/ManagerTest.php
@@ -1,16 +1,18 @@
 <?php
-namespace DrestTests;
+namespace DrestTests\Functional;
 
+use DrestTests\DrestFunctionalTestCase;
 
-
-class ManagerTest extends DrestTestCase
+class ManagerTest extends DrestFunctionalTestCase
 {
 
     public function testExecutingOnANamedRoute()
     {
         $dm = $this->_getDrestManager();
 
-        $dm->dispatch()
+
+        $response = $dm->dispatch(null, null, 'DrestTests\Entities\Typical\User::get_user');
+
 
     }
 }
