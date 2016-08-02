0175e4092f45b7c89d661d79811d45b30a7b7fb0
Lee Davis <leedavis81@hotmail.com>
2016-08-02 17:13:36 +0100

    Allow action exceptions to cascade up

---
+++ src/Drest/Service/Action/ActionException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Drest\Service\Action;
+
+/**
+ * Extensible exception that will allow error messages to cascade back to the client
+ * @package Drest\Service\Action
+ */
+class ActionException extends \Exception {}
